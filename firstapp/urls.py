3db659dcf5c91c58412be9352ad81b77c21b813e
srikanth <srikanth123poola@gmail.com>
2023-11-30 11:21:09 +0530

    templates

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from firstapp.views import string,html,jsondata,dict
+from firstapp.views import string,html,jsondata,dict,greeting
 
 urlpatterns = [
     path('string',string),
     path('html',html),
     path('jsondata',jsondata),
-    path('dict',dict)
+    path('dict',dict),
+    path('greeting',greeting)
 ]
--- firstapp/views.py
+++ firstapp/views.py
@@ -1,6 +1,7 @@
 import json
 from django.shortcuts import render
 from django.http import HttpResponse
+from firstapp.models import Employee
 
 # Create your views here.
 def string(request):
@@ -12,3 +13,12 @@ def jsondata(request):
     return HttpResponse(json_object)
 def dict(request):
     return HttpResponse({'msg':"This is dictionary datatype"})
+
+def greeting(request):
+    employees = Employee.objects.all()
+    employee_list = []
+    for employee in employees:
+        employee_list.append(employee.name)
+    context={'employee':employee_list}
+    print(context)
+    return render(request,'firstapp/index.html',context)
