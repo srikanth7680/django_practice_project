f5e70733f7e9fd869604043e238865546b692f84
srikanth <srikanth123poola@gmail.com>
2023-12-08 13:08:06 +0530

    crud-drf-commit

--- firstapp/apis.py
+++ firstapp/apis.py
@@ -2,6 +2,10 @@ from firstapp.serializers import EmployeeListSerializer
 from firstapp.models import Employee
 from rest_framework import generics
 
-class EmployeeList(generics.ListAPIView):
+class EmployeeList(generics.ListCreateAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeListSerializer
+
+class EmployeeRetrieve(generics.RetrieveAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeListSerializer
